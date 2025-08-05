0e2c437967615b0707323b4c0002656d266d6953
Peter Shuvalov <frament@mail.ru>
2025-08-05 21:58:39 +0500

    app init providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions
+} from '@angular/router';
 import { routes } from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
@@ -9,8 +18,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+    ),
     provideAnimationsAsync(),
-    providePrimeNG({theme: {preset: Aura}})
+    providePrimeNG({theme: {preset: Aura}}),
+    provideAppInitializer(() => {}),
   ]
 };
