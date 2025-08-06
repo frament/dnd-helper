c7458e4d4f42f811085c1e54333b233e8758744d
Peter Shuvalov <frament@mail.ru>
2025-08-06 23:11:36 +0500

    add db service

---
+++ src/app/database.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Surreal} from "surrealdb";
+export type LiveActionType = "CREATE" | "UPDATE" | "DELETE" | "CLOSE";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Database {
+  public db = new Surreal();
+  async init():Promise<void>{
+    await this.db.connect(`http://${location.hostname}:8000/rpc`, {namespace:'dnd', database: 'dnd'});
+  }
+
+  async select<T>(query: string, vars?: Record<string, unknown>): Promise<T[]>{
+    return (await this.db.query<[Array<T & Record<any, any>>]>(query, vars))[0] ?? [];
+  }
+  async selectOne<T>(query: string, vars?: Record<string, unknown>): Promise<T|null>{
+    return (await this.select<T>(query, vars))[0] ?? null;
+  }
+}
